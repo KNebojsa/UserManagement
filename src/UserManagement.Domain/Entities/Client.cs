04a9ac4336503bf856328fdcc4360ba820694fbc
Nebojsa Kovacic <Nele25@live.com>
2025-09-03 17:49:07 +0200

    Add entities - Client and User

---
+++ src/UserManagement.Domain/Entities/Client.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UserManagement.Domain.Entities
+{
+    public class Client
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public required User User { get; set; }
+
+        public required Guid UserId { get; set; }
+
+        public required string ApiKey { get; set; }
+
+        public required DateTime CreatedAt { get; set; }
+    }
+}
---
+++ src/UserManagement.Domain/Entities/User.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagement.Domain.Entities
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [MinLength(3), MaxLength(30)]
+        public required string UserName { get; set; }
+
+        [MinLength(2), MaxLength(512)]
+        public required string PasswordHash { get; set; }
+
+        [MinLength(2), MaxLength(30)]
+        public required string FirstName { get; set; }
+
+        [MinLength(2), MaxLength(30)]
+        public required string LastName { get; set; }
+
+        [MinLength(2), MaxLength(50)]
+        public required string Email { get; set; }
+
+        [MinLength(2), MaxLength(30)]
+        public required string MobileNumber { get; set; }
+
+        [MinLength(2), MaxLength(10)]
+        public string? Language { get; set; }
+
+        [MinLength(2), MaxLength(10)]
+        public string? Culture { get; set; }
+
+        public required IEnumerable<Client> Clients { get; set; }
+
+        public required DateTime DateCreated { get; set; }
+
+        public DateTime? DateModified { get; set; }
+
+
+        public User()
+        {
+            DateCreated = DateTime.UtcNow;
+        }
+
+        public void MarkModified()
+        {
+            DateModified = DateTime.UtcNow;
+        }
+    }
+}
